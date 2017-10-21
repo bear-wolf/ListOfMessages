74e88a18e8b7a26a34f9d3abe66469658da80ffa
bear-wolf <bear-wolf@ukr.net>
2017-10-21 18:55:09 +0300

    back: add base ctrl

--- server/controller/translateController.js
+++ server/controller/translateController.js
@@ -1,3 +1,6 @@
+var util = require('util'),
+    baseCtrl = require('./baseController');
+
 var mongoController = require('./mongoController.js'),
     fileOfData = 'data.json',
     enumMessage = require('./../config/message'),
@@ -5,11 +8,13 @@ var mongoController = require('./mongoController.js'),
     fs = require('fs');
 
 var translateController = function (resource) {
+    translateController.super_.apply(this, arguments);
     this.resource = resource;
-    this.mongoCtrl = (new mongoController);
     this.fs = fs;
     this.entity = 'translate';
 }
+
+util.inherits(translateController, baseCtrl);
 
 translateController.prototype.index = function () {
     this.resource.writeHead(200);
@@ -17,95 +22,51 @@ translateController.prototype.index = function () {
 }
 
 translateController.prototype.save = function (json) {
-    var _data = {},
-        _this = this;
+    var _this = this;
 
-    this.mongoCtrl.connect(function (data) {
-        if (!data.status) {
-            _this.resource.end(404);
-            return;
-        }
-        var collection = data.db.collection(_this.entity);
+    this.saveToDB(_this.entity, json, function (data) {
+        var _data = {};
 
-        var callback = function (err, result) {
-            if (err) {
-                _data.status = false;
-                delete _data.message;
-            } else{
-                _data.status = true;
-            }
-
-            data.db.close();
-
-            _this.resource.end(JSON.stringify(_data));
-        }
-
-        if (json.id) {
-            // update
-            _data.message = enumMessage.updateSuccessful;
-            collection.updateOne( {
-                    '_id': json.id
-                },
-                {
-                    $set: json
-                },
-                true, callback
-            );
+        if (data.error) {
+            _data.status = false;
+            _data.message = data.message;
         } else {
-            _data.message = enumMessage.insertSuccessful;
-            collection.insertOne(json, callback); //insert
+            _data.status = true;
         }
+        _this.resource.end(JSON.stringify(_data));
     });
 }
 
 translateController.prototype.get = function () {
     var _this = this;
 
-    this.mongoCtrl.connect(function (data) {
-        var _data;
+    this.getFromDB(_this.entity, function (data) {
+        var _data = {};
 
-        _data = {
-            count: 0
-        };
-
-        if (!data.status) {
-            _this.resource.end(JSON.stringify(data));
-            return;
+        if (data.error) {
+            _data.status = false;
+            _data.message = data.message;
+        } else {
+            _data.status = true;
+            _data.body = data.body;
+            _data.count = data.count;
         }
-        var collection = data.db.collection(_this.entity);
-
-        collection.find().toArray(function(err, docs) {
-            if (err) {
-                _data.status = false;
-            } else {
-                _data.status = true;
-                _data.body = docs;
-                _data.count = docs.length
-            }
-
-            data.db.close();
-            _this.resource.end(JSON.stringify(_data));
-        });
+        _this.resource.end(JSON.stringify(_data));
     });
 }
 
 translateController.prototype.getById = function (id) {
     var _this = this;
 
-    this.mongoCtrl.connect(function (data) {
-        var _data;
-
-        _data = {
-            count: 0
-        };
+    this.connectToDB(_this.entity, function (data) {
+        var _data = {};
 
         if (!data.status) {
-            _this.resource.end(JSON.stringify(data));
-            return;
+            _data.status = false;
+            _this.resource.end(JSON.stringify(_data));
         }
-        var collection = data.db.collection(_this.entity);
 
-        collection.find({
+        data.collection.find({
             '_id' : new _this.mongoCtrl.mongodb.ObjectID(id)
         }).toArray(function(err, docs) {
             if (err) {
@@ -119,7 +80,7 @@ translateController.prototype.getById = function (id) {
             data.db.close();
             _this.resource.end(JSON.stringify(_data));
         });
-    });
+    })
 }
 
 
